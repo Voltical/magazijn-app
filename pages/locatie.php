6f53959f33273374cf55ce6d03c28ece29485ebb
SennaMystic <524917@vistacollege.nl>
2025-10-21 13:28:32 +0200

    dikke update jonge

--- pages/locatie.php
+++ pages/locatie.php
@@ -1,21 +1,45 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="nl">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="../assets/css/style.css">
-    <title>Maak een account - Vista College</title>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="../assets/css/locatie.css">
+  <title>Locatie Kiezen - Vista College</title>
 </head>
 <body>
-    <div class="left-container">
-        <div class="bg"></div>
-    </div>
+  
+  <div class="left-container">
+    <div class="bg"></div>
 
-    <div class="right-panel">
-        <div class="content">0
-            <h1>Welkom bij het magazijnportaal!</h1>
-            <p>bij je eerste keer inloggen vragen we je om je schoollactie te kiezen. Welke locatie van jouw opleiding wil je koppelen aan je account</p>
-        </div>
+    <div class="overlay-text">
     </div>
+  </div>
+
+  
+  <div class="right-panel">
+    <div class="content">
+      <h1>Welkom bij het Magazijnportaal!</h1>
+      
+      
+      <p>
+        Bij je eerste keer inloggen vragen we je om je schoollocatie te kiezen.
+        Welke locatie van jouw opleiding wil je koppelen aan je account?
+      </p>
+        <p class="note">Je kan dit later nog aanpassen in je gebruikersinstellingen</p>
+        
+
+        <form>
+        <select required>
+          <option value="" disabled selected>Selecteer</option>
+          <option value="heerlen">Heerlen</option>
+          <option value="sittard">Sittard</option>
+          <option value="maastricht">Maastricht</option>
+        </select>
+
+        <button type="submit">inloggen</button>
+      </form>
+      
+    </div>
+  </div>
 </body>
 </html>
