a1602f869bcf1a59703516dae8c6b6fd0919bd9c
SennaMystic <524917@vistacollege.nl>
2025-10-22 11:05:00 +0200

    magazijn pagina update

---
+++ pages/magazijn.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../assets/css/magazijn.css">
+    <title>Magazijn</title>
+</head>
+<body>
+      <div class="left-container">
+    <div class="bg"></div>
+    <div class="buttons"></div>
+    <button>Dashboard</button>
+    <button>Placeholder</button>
+    <button>Opslag</button>
+    <button>Grafiek</button>
+</div>
+
+</body>
+</html>
