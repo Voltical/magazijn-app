2bba9e3101f9a07ac7aec32fe92f1179ef2b0033
SennaMystic <524917@vistacollege.nl>
2025-10-28 09:20:42 +0100

    pages

---
+++ public/pages voor magazijn/grafiek.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="nl">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="../assets/css/magazijn.css">
+  <title>Grafiek</title>
+</head>
+<body>
+  <div class="left-container">
+    <div class="sidebar-buttons"> 
+      <a href="#" class="sidebar-btn">
+        <img src="../assets/images/huis.png" class="icon" alt="huis icoon">
+        Dashboard
+      </a>
+
+      <a href="#" class="sidebar-btn">
+        <img src="../assets/images/monitor.png" class="icon" alt="monitor icoon">
+        Placeholder
+      </a>
+
+      <a href="#" class="sidebar-btn">
+        <img src="../assets/images/doos.png" class="icon" alt="doos icoon">
+        Opslag
+      </a>
+
+      <a href="#" class="sidebar-btn">
+        <img src="../assets/images/grafiek.png" class="icon" alt="grafiek icoon">
+        Grafiek
+      </a>
+    </div>
+</body>
+</html>
