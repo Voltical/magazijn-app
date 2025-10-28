5e7f48eacb3422f4e0fe8811532ad02f8e84b7c2
SennaMystic <524917@vistacollege.nl>
2025-10-28 09:14:11 +0100

    update bigga

--- pages/magazijn.php
+++ pages/magazijn.php
@@ -1,28 +1,33 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="nl">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="../assets/css/magazijn.css">
-    <title>Magazijn</title>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="../assets/css/magazijn.css">
+  <title>Magazijn</title>
 </head>
 <body>
-      <div class="left-container">
+  <div class="left-container">
     <div class="sidebar-buttons"> 
-    
-  <a href="#" class="sidebar-btn"><i class="fas fa-lock"></i> Dashboard</a>
-  <a href="#" class="sidebar-btn"><i class="fas fa-desktop"></i> Placeholder</a>
-  <a href="#" class="sidebar-btn"><i class="fas fa-box"></i> Opslag</a>
-  <a href="#" class="sidebar-btn"><i class="fas fa-chart-line"></i> Grafiek</a>
-</div>
+      <a href="#" class="sidebar-btn">
+        <img src="../assets/images/huis.png" class="icon" alt="huis icoon">
+        Dashboard
+      </a>
 
-<div class="logo-container">
-<img src="../assets/images/huis.png" class="logo" id="huis"alt="logo huisje">
-<img src="../assets/images/monitor.png" class="logo" alt="logo monitor">
-<img src="../assets/images/doos.png" class="logo" alt="logo doos">
-<img src="../assets/images/grafiek.png"class="logo" alt="logo grafiek">
-  </div>
+      <a href="#" class="sidebar-btn">
+        <img src="../assets/images/monitor.png" class="icon" alt="monitor icoon">
+        Placeholder
+      </a>
 
+      <a href="#" class="sidebar-btn">
+        <img src="../assets/images/doos.png" class="icon" alt="doos icoon">
+        Opslag
+      </a>
 
+      <a href="#" class="sidebar-btn">
+        <img src="../assets/images/grafiek.png" class="icon" alt="grafiek icoon">
+        Grafiek
+      </a>
+    </div>
 </body>
 </html>
